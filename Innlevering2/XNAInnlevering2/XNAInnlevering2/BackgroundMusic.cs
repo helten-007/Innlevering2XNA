9665df18dd17b1923a363087ac9892a03218c148
Hans Kristian Kirkemo <kirhan12@nith.no>
2013-04-11 14:08:11 +0200

    Added classes SoundEffects and BackgroundMusic

---
+++ Innlevering2/XNAInnlevering2/XNAInnlevering2/BackgroundMusic.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace XNAInnlevering2
+{
+    /// <summary>
+    /// Class that handles the background music in the game
+    /// </summary>
+    class BackgroundMusic
+    {
+        private ContentManager Content;
+        KeyboardState _previousKeyState;
+        bool songIsPlaying;
+
+        public BackgroundMusic(ContentManager content)
+        {
+            Content = content;
+
+            ///Gathers songs from the users computer
+            MediaLibrary library = new MediaLibrary();
+            SongCollection songs = library.Songs;
+            Song song = songs[0];
+            MediaPlayer.Play(songs);
+            songIsPlaying = true;
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            KeyboardState _currentKeyState = Keyboard.GetState();
+
+            if (_currentKeyState.IsKeyDown(Keys.Q) == true && _previousKeyState.IsKeyDown(Keys.Q) == false)
+            {
+                if (songIsPlaying == false)
+                {
+                    MediaPlayer.Resume();
+                    songIsPlaying = true;
+                }
+                else
+                {
+                    MediaPlayer.Pause();
+                    songIsPlaying = false;
+                }
+            }
+
+            if (_currentKeyState.IsKeyDown(Keys.W) == true && _previousKeyState.IsKeyDown(Keys.W) == false)
+            {
+                MediaPlayer.MoveNext();
+            }
+
+            if (_currentKeyState.IsKeyDown(Keys.A) == true && _previousKeyState.IsKeyDown(Keys.A) == false)
+            {
+                MediaPlayer.Volume += 0.1f;
+                Console.WriteLine(MediaPlayer.Volume);
+            }
+
+            if (_currentKeyState.IsKeyDown(Keys.S) == true && _previousKeyState.IsKeyDown(Keys.S) == false)
+            {
+                MediaPlayer.Volume -= 0.1f;
+                Console.WriteLine(MediaPlayer.Volume);
+            }
+            _previousKeyState = _currentKeyState;
+        }
+    }
+}
---
+++ Innlevering2/XNAInnlevering2/XNAInnlevering2/SoundEffects.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace XNAInnlevering2
+{
+    /// <summary>
+    /// Class that handles the sound effects in the game
+    /// </summary>
+    public class SoundEffects
+    {
+
+        private SoundEffect soundGameStart, soundGameOver, soundFoodSpawn;
+
+        private ContentManager Content;
+
+
+
+        public SoundEffects(ContentManager content)
+        {
+            Content = content;
+
+            soundGameOver = Content.Load<SoundEffect>("Audio\\Effects\\Applause");
+            soundGameStart = Content.Load<SoundEffect>("Audio\\Effects\\Air Horn");
+            soundFoodSpawn = Content.Load<SoundEffect>("Audio\\Effects\\Door Bell");
+        }
+
+        public void Update(GameTime gameTime)
+        {
+
+        }
+
+
+        public void playFoodSpawn()
+        {
+            soundFoodSpawn.Play();
+        }
+
+        public void playSoundGameOver()
+        {
+            soundGameOver.Play();
+        }
+
+        public void playSoundGameStart()
+        {
+            soundGameStart.Play();
+        }
+
+    }
+
+}
